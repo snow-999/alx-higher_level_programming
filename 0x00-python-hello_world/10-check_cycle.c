75e5b8e4d19742643ab4c32f59c15455ab8a59e4
khaled <khaledleader012@gmail.com>
2023-10-30 10:16:04 -0700

    m

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include "stdio.h"
+#include "stdlib.h"
+/**
+ * check_cycle - check list is ceycled
+ * @list: list will be cheacked
+ * Return: 1 if equql, 0 or not
+ *
+ *
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *str = list, listint_t *ptr = list;
+
+	while (ptr && ptr->next)
+	{
+		str = str->next;
+		ptr = ptr->next->next;
+		if (str == ptr)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
