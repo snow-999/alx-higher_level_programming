c4202be0007697e45c707b902a33b97835de0cac
khaled <khaledleader012@gmail.com>
2023-10-31 10:20:10 -0700

    m

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,6 +6,6 @@ def fizzbuzz():
         elif i % 5 == 0 and i % 3 != 0:
             print("Buzz", end=" ")
         elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz",end=" ")
         else:
             print("{:d}".format(i), end=" ")
