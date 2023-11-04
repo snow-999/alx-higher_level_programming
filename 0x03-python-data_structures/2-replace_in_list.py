f5d32117a70569667ac51d0c2c92fedaca534fcd
khaled <khaledleader012@gmail.com>
2023-11-04 09:07:10 -0700

    m

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,9 @@
 def replace_in_list(my_list, idx, element):
     if not my_list:
         return None
-    if idx < 0 or idx > len(my_list):
+    if idx < 0:
+        return None
+    if idx > len(my_list):
         return None
     my_list[idx] = element
     return my_list
