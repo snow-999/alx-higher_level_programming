bd2315d3b87d61c591b429bb21b1c78df5713064
khaled <khaledleader012@gmail.com>
2023-11-29 08:10:52 -0800

    m

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
     def __str__(self):
         """string presentation"""
         string = ""
-        if self.__height != 0 or self.__width != 0:
+        if self.__height != 0 and self.__width != 0:
             string += "\n".join("#" * self.__width
                                 for i in range(self.__height))
         return string
