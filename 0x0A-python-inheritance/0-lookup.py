181aa6ea063677fd124a5a4e17d87a0f44fe1ecd
khaled <khaledleader012@gmail.com>
2023-12-04 04:24:09 -0800

    m

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""function to return the list"""
+
+
 def lookup(obj):
+    """look up function
+    arg:
+        obj: the object that will get the docmnebts inside
+    return: the doucments"""
     return dir(obj)
