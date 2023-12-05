1e69cfcb97180b1b99d63ef5bda3f61fe21af394
khaled <khaledleader012@gmail.com>
2023-12-05 03:42:42 -0800

    m

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """read file function"""
-    with open(filename, encoding="uft-8") as new_file:
-        print(new_file.read(), end="")
+    with open(filename, encoding="uft-8") as f:
+        print(f.read(), end="")
